78adf3afd2c0e70cfada67d067b411046fa6d417
JamterFondy <jedwind777@gmail.com>
2026-01-08 21:10:01 +0900

    始まりのコミット

---
+++ Just2DGame/Assets/Scripts/SquareUI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SquareUI : MonoBehaviour
+{
+    public GameObject obj;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.CompareTag("Square"))
+        {
+            obj.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        }
+        else
+        {
+            obj.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+    }
+
+    
+}
