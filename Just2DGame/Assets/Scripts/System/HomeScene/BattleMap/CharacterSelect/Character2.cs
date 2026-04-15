5a021f9add1e083e74800d4d3e9f899e9c325d32
JamterFondy <jedwind777@gmail.com>
2026-04-15 09:46:25 +0900

    マウス操作による選択キャラアイコンの変化の実装
    テストとして２キャラを想定。現在は画像がないためオブジェクトのSpriteのカラー変更で変化の可視化を行っている。

---
+++ Just2DGame/Assets/Scripts/System/HomeScene/BattleMap/CharacterSelect/Character2.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Character2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] SelectCharacter selectCharacter;
+
+    int storedOriginalID;
+    bool hasStoredOriginal;
+    bool isPointerOver;
+    bool isPointerDown;
+
+    void Start()
+    {
+        // Inspector に未割り当てなら近くの SelectCharacter を自動で探す
+        if (selectCharacter == null)
+        {
+            selectCharacter = GetComponentInParent<SelectCharacter>();
+            if (selectCharacter == null)
+            {
+                selectCharacter = FindObjectOfType<SelectCharacter>();
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        storedOriginalID = selectCharacter.characterID;
+
+        isPointerOver = true;
+        TrySetToTwo();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPointerDown = true;
+        TrySetToTwo();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        // カーソルがボタン上にないか、または現在 characterID が 2 でないなら復元
+        if (!isPointerOver || (selectCharacter != null && selectCharacter.characterID != 2))
+        {
+            RestoreOriginal();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerOver = false;
+        // 押されていない状態なら復元（または外部で characterID が変わっていれば Update 側で復元される）
+        if (!isPointerDown || (selectCharacter != null && selectCharacter.characterID != 2))
+        {
+            RestoreOriginal();
+        }
+    }
+
+    void TrySetToTwo()
+    {
+        if (selectCharacter == null) return;
+
+        selectCharacter.characterID = 2;
+    }
+
+    void RestoreOriginal()
+    {
+        if (selectCharacter == null) return;
+
+        selectCharacter.characterID = storedOriginalID;
+    }
+}
