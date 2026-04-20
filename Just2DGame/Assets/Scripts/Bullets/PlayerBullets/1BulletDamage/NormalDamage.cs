5284f3870c2901f40d84ca2040cbb20d75e49eea
JamterFondy <jedwind777@gmail.com>
2026-04-20 13:25:32 +0900

    EnemyStatusがダメージを受けるように最新の実装状態に合わせた

--- Just2DGame/Assets/Scripts/Bullets/PlayerBullets/1BulletDamage/NormalDamage.cs
+++ Just2DGame/Assets/Scripts/Bullets/PlayerBullets/1BulletDamage/NormalDamage.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class DamageManager : MonoBehaviour
+public class NormalDamage : MonoBehaviour
 {
     [SerializeField] PlayerStatus playerStatus;
 
--- Just2DGame/Assets/Scripts/Enemy/EnemyStatus.cs
+++ Just2DGame/Assets/Scripts/Enemy/EnemyStatus.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class EnemyStatus : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+
     public int hp = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -17,11 +19,46 @@ public class EnemyStatus : MonoBehaviour
         
     }
 
-    public void ApplyDamage(int amount)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        hp -= amount;
-        Debug.Log($"{gameObject.name} took {amount} damage. HP = {hp}");
-        if (hp <= 0) Die();
+        PlayerStatus playerStatus = collision.GetComponent<PlayerStatus>();
+          
+        if (collision.CompareTag("Bullet"))
+        {
+            int damage = 0;
+            // ChainDamageなど、弾の種類ごとにダメージを取得
+            var normalDamage = collision.GetComponent<NormalDamage>();
+            var chainDamage = collision.GetComponent<ChainDamage>();
+            var lastChainDamage = collision.GetComponent<LastChainDamage>();
+
+            if (normalDamage != null)
+            {
+                damage = chainDamage.GetDamage();
+            }
+            else if (chainDamage != null)
+            {
+                damage = chainDamage.GetDamage();
+            }
+            else if (lastChainDamage != null)
+            {
+                damage = lastChainDamage.GetDamage();
+            }
+            // 他の弾種も同様にGetDamage()を持たせておけば拡張可能
+
+            if (damage > 0)
+            {
+                ApplyDamage(damage);
+            }
+        }
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Die()
