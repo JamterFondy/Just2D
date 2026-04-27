483beca4ecfcff7407dcd1130d28a0acf31c146f
JamterFondy <jedwind777@gmail.com>
2026-04-27 13:17:54 +0900

    上のタブにHomeSceneに戻るボタンを追加

---
+++ Just2DGame/Assets/Scripts/System/AllScene/UpperTabs/GoBackHome.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GoBackHome : MonoBehaviour
+{
+    LoadingManager loadingManager;
+    UIManager uiManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        loadingManager = FindObjectOfType<LoadingManager>();
+        uiManager = FindObjectOfType<UIManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if(uiManager.currentScene == SceneType.Home)
+        {
+            return; // Ç∑Ç≈Ç…ÉzÅ[ÉÄÇ…Ç¢ÇÈèÍçáÇÕâΩÇ‡ÇµÇ»Ç¢
+        }
+        else
+        {
+            loadingManager = FindObjectOfType<LoadingManager>();
+
+            uiManager.currentScene = SceneType.Home;
+            uiManager.currentState = UIState.HomeDefault;
+
+            loadingManager.StartCoroutine(loadingManager.LoadSceneWithLoadingScreen("LoadingScene", "HomeScene"));
+        }
+
+        
+    }
+}
