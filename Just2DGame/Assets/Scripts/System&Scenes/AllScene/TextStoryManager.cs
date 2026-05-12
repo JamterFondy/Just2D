0cda3c8da3bd50e37ac2fee78d7243716ca72496
JamterFondy <jedwind777@gmail.com>
2026-05-12 14:30:56 +0900

    Story_0.jsonに変更を追加。テキストイベントのマネージャーに変更を追加。

--- Just2DGame/Assets/Scripts/System&Scenes/AllScene/TextStoryManager.cs
+++ Just2DGame/Assets/Scripts/System&Scenes/AllScene/TextStoryManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
 
 public class TextStoryManager : MonoBehaviour
 {
@@ -6,6 +8,19 @@ public class TextStoryManager : MonoBehaviour
 
     public int StoryCount = 0; //ストーリーの進捗度。テキストのまとまりを管理する。
     public int TextPage = 1; //テキストの進捗度。１ストーリー内におけるテキストの進捗を管理する。StoryCountが変動すると初期値に戻る。
+
+
+    [System.Serializable]
+    public class Story
+    {
+        public int TextNum;
+        public string Character;
+        public string Text;
+        public string Transition;
+        public string Image;
+        public string BGM;
+    }
+
 
     void Awake()
     {
