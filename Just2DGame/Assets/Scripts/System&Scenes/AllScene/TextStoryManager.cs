805fc861f8617896636d94b76b93676fdc564567
JamterFondy <jedwind777@gmail.com>
2026-05-12 12:59:13 +0900

    ストーリーを管理するマネージャーのスクリプト

--- Just2DGame/Assets/Scripts/System&Scenes/AllScene/TextStoryManager.cs
+++ Just2DGame/Assets/Scripts/System&Scenes/AllScene/TextStoryManager.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class TextStoryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    UIManager uiManager;
+
+    public int StoryCount = 0; //ストーリーの進捗度。テキストのまとまりを管理する。
+    public int TextPage = 1; //テキストの進捗度。１ストーリー内におけるテキストの進捗を管理する。StoryCountが変動すると初期値に戻る。
+
+    void Awake()
+    {
+        uiManager = FindAnyObjectByType<UIManager>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
