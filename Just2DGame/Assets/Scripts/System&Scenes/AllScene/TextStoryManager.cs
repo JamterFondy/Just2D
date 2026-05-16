9c0b055b96bc2e5a4fc8dd4eeb521e1c145c39c9
JamterFondy <jedwind777@gmail.com>
2026-05-16 21:13:33 +0900

    TextStoryManager.csの途中経過

--- Just2DGame/Assets/Scripts/System&Scenes/AllScene/TextStoryManager.cs
+++ Just2DGame/Assets/Scripts/System&Scenes/AllScene/TextStoryManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
@@ -14,8 +15,25 @@ public class TextStoryManager : MonoBehaviour
 
     private StoryLayout storyLayout;
 
+    Sprite currentCharacter, currentTransition, currentImage;
+    AudioClip currentBGM;
+
     public int storyNum = 0; // ストーリーの進捗度。テキストのまとまりを管理する。
     public int textPage = 1; // テキストの進捗度。１ストーリー内におけるテキストの進捗を管理する。StoryCountが変動すると初期値に戻る。
+
+    // UI系統のゲームオブジェクトのアタッチ
+    [SerializeField] SpriteRenderer charaIconObj;
+    [SerializeField] TextMeshProUGUI storyTextObj;
+    [SerializeField] SpriteRenderer transitionImageObj;
+    [SerializeField] SpriteRenderer backgroundImageObj;
+    BGManager bgManager;
+
+
+    // 使用する全リソース（素材）のアタッチをするリスト
+    [SerializeField] Sprite[] Characters;
+    [SerializeField] Sprite[] Transitions;
+    [SerializeField] Sprite[] Images;
+    [SerializeField] AudioClip[] BGMs;
 
 
     [System.Serializable]
@@ -39,7 +57,10 @@ public class TextStoryManager : MonoBehaviour
     void Awake()
     {
         uiManager = FindAnyObjectByType<UIManager>();
+        bgManager = FindAnyObjectByType<BGManager>();
     }
+
+
 
     void Start()
     {
@@ -51,21 +72,13 @@ public class TextStoryManager : MonoBehaviour
     }
 
 
+
     public void StoryMode(bool IsStoryMode)
     {
         if (IsStoryMode) LoadStory();
     }
 
-    public void UpdateStoryCount()
-    {
-        // ストーリーの進捗は＋１
-        storyNum = PlayerPrefs.GetInt("StoryNum") + 1;
-        PlayerPrefs.SetInt("StoryNum", storyNum);
 
-        // テキストページは初期化（1になる）
-        textPage = 0;
-        PlayerPrefs.SetInt("TextPage", 1);
-    }
 
     void LoadStory()
     {
@@ -77,6 +90,8 @@ public class TextStoryManager : MonoBehaviour
         // 次にテキスト・キャラクター・テキストなどの情報をTextNumに従って受け取る処理を動かす。
         StoryCycle(storyLayout);
     }
+
+
 
     void StoryCycle(StoryLayout storyLayout)
     {
@@ -97,7 +112,6 @@ public class TextStoryManager : MonoBehaviour
         }
 
 
-
         // 最後のTextNumかどうかを判断し、結果に応じて処理を変えられるようにDisplay～関数にboolを引き渡す。
         if(storyLayout.layout.Any(p => p != null && p.TextNum > textPage))
         {
@@ -115,6 +129,7 @@ public class TextStoryManager : MonoBehaviour
     }
 
 
+
     IEnumerator DisplayStoryLayouts(StoryLayouts entry, bool IsEndOfStory)
     {
         // 受け取った情報の種類ごとに変数を用意し格納。
@@ -125,6 +140,51 @@ public class TextStoryManager : MonoBehaviour
         string bgm = entry.BGM;
 
 
+        // 次にImage画像系はリストの中から今回の格納データと名前一致するものを取得する。
+        foreach (var c in Characters)
+        {
+            if (c.name == character) currentCharacter = c;
+        } 
+
+        foreach (var t in Transitions)
+        {
+            if (t.name == transition) currentTransition = t;
+        }
+
+        foreach (var i in Images)
+        {
+            if (i.name == image) currentImage = i;
+        }
+
+        foreach (var b in BGMs)
+        {
+            if (b.name == bgm) currentBGM = b;
+        }
+
+
+        // そして各オブジェクトのSpriteやテキストを変更する。
+        // ToDo BGMはBGManagerに直接受け取った内容を受け渡す形になる。UIManagerのSceneEventの状態（T or F）に応じてBGM変化方法を変えた後、AudioClipを受け取る変数をBGManager内に作るなど。
+        charaIconObj.sprite = currentCharacter;
+        storyTextObj.text = text;
+        transitionImageObj.sprite = currentTransition;
+        backgroundImageObj.sprite = currentImage;
+
+
         yield return null;
+    }
+
+
+
+    public void UpdateStoryNum()
+    {
+        // ストーリーの進捗は＋１
+        storyNum = PlayerPrefs.GetInt("StoryNum") + 1;
+        PlayerPrefs.SetInt("StoryNum", storyNum);
+        PlayerPrefs.Save();
+
+        // テキストページは初期化（1になる）
+        textPage = 0;
+        PlayerPrefs.SetInt("TextPage", 1);
+        PlayerPrefs.Save();
     }
 }
