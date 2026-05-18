f672341d0bac6cfade0432398bc0ee9dcf47f24c
JamterFondy <jedwind777@gmail.com>
2026-05-18 10:55:44 +0900

    PlayerPrefsEvent.csの途中経過

--- Just2DGame/Assets/Scripts/System&Scenes/AllScene/PlayerPrefsEventManager.cs
+++ Just2DGame/Assets/Scripts/System&Scenes/AllScene/PlayerPrefsEventManager.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class PlayerPrefsEventManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // PlayerPrefsの値に応じてイベントの発火をつかさどるクラス。
+    // シーン別に起きうるイベントをPlayerPrefsの値と照らし合わせてシーン遷移が行われたタイミングで呼び出されるようにする。
+    // 例) ホームシーンではStoryNum = 0,3,6...でストーリーが、StoryNum = 1,4,5...でチュートリアルのイベントがある。
+    // シーン遷移かUIStateなどの状態変遷をトリガーにイベントごとの関数を呼び出す。
+    public void StartStoryEvent()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
